dd90fee91b533791a3582a264d20a47d5e462950
Munif <almenwer.munif@gmail.com>
2021-06-09 22:12:36 +0300

    add tweets index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,9 +3,24 @@ class TweetsController < ApplicationController
 
     def index
         @tweets = Current.user.tweets
-    end
-
+    end      
+  
     def new
         @tweet = Tweet.new
+    end
+
+    def create
+        @tweet = Current.user.tweets.new(tweet_params)
+        if @tweet.save
+            redirect_to tweets_path, notice: "Tweet was scheduled successfully"
+        else 
+            render :new
+        end
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
     end
 end
